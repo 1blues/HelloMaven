29c7046254fb059832d53f7477242d0d8a38097b
1blues <66257503+1blues@users.noreply.github.com>
2020-10-03 16:59:38 +0500

    Add files via upload

---
+++ kalugin/src/main/java/ru/kalugin/App.java
@@ -0,0 +1,11 @@
+package ru.kalugin;
+
+public class App 
+{
+    public App() {
+    }
+    public static void main( String[] args )
+    {
+        System.out.println(new AppBean("Maven").sayHello());
+    }
+}
---
+++ kalugin/src/main/java/ru/kalugin/AppBean.java
@@ -0,0 +1,13 @@
+package ru.kalugin;
+
+public class AppBean 
+{
+    private String text;
+ 
+    public AppBean(final String text) {
+        this.text = text;
+    } 
+    public String sayHello() {
+        return "Hello, " + text;
+    } 
+}
